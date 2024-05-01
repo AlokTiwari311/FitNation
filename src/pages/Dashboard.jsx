b6828bb15873e9e76ac38bedd6386e0d32ecd863
AlokTiwari311 <AlokTiwari311113@gmail.com>
2024-05-02 01:50:39 +0530

    first commit

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+// import { VortexDemo } from "../components/VortexDemo";
+
+const Dashboard = () => {
+  return (
+    <div className="flex justify-center items-center text-white text-3xl h-full">
+      {/* <VortexDemo /> */}
+    </div>
+  );
+};
+
+export default Dashboard;
